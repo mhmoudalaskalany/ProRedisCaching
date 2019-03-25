9e2a6bdae8c890a4840fb6cdb67990149229c31a
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2019-03-25 23:35:26 +0200

    Add project files.

---
+++ ProRedisCaching/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProRedisCaching.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
